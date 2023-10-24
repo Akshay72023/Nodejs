12ee74932220ec69516fc396f317beee5ec1ef30
Akshay72023 <al7864745@gmail.com>
2023-10-24 22:20:13 +0530

    displaying data on screen

--- routingrequest.js
+++ routingrequest.js
@@ -5,34 +5,44 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
+    fs.readFile("message.txt",{encoding:"utf-8"},(err,data)=>{
+      if(err){
+        console.log(err);
+      }
+      res.write('<html>');
+      res.write('<head><title>Enter Message</title><head>');
+      res.write(`<body>${data}</body>`)
+      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+      res.write('</html>');
+      return res.end();
+    });  
   }
-  if (url === '/message' && method === 'POST') {
+  else if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', (chunk) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
       console.log(message);
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message,(err)=>{
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+    
   }
+  else{
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
+  }
 });
 
 server.listen(3001);
