44c74c051f282ce3dade59f08f14273a6f76a739
Akshay72023 <al7864745@gmail.com>
2023-10-25 13:47:20 +0530

    autorestart

--- response.js
+++ response.js
@@ -1,13 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  // Get the requested URL from the request object
+
   const url = req.url;
 
-  // Set Content-Type to plain text
   res.setHeader('Content-Type', 'text/plain');
 
-  // Handle different URLs and send custom responses
   if (url === '/home') {
     res.end('Welcome home');
   } else if (url === '/about') {
@@ -15,7 +13,6 @@ const server = http.createServer((req, res) => {
   } else if (url === '/node') {
     res.end('Welcome to my Node Js project');
   } else {
-    // Handle unknown URLs with a 404 response
     res.statusCode = 404;
     res.end('404 - Not Found');
   }
